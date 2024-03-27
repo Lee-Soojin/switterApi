c1ddb3230790176c46a3098b9753d99d5d966373
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-03-27 18:34:22 +0900

    feat: Set up cors options

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: ["http://localhost:3000/"],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
-app.use(cors());
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.listen(8080);
