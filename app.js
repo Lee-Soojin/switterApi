27f84f780e41443bd2ceccacd15c5a124f3b3aed
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-05-01 10:14:46 +0900

    feat: Update tweet list

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const timelineFilePath = path.join(tweetsDir, "timeline.json");
 if (!fs.existsSync(tweetsDir)) fs.mkdirSync(tweetsDir);
 
 const corsOptions = {
-  origin: ["http://localhost:3000/"],
+  origin: ["http://localhost:3000"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
@@ -36,11 +36,9 @@ app.get("/tweets", (req, res) => {
     fs.readFile(userFilePath, "utf-8", (err, data) => {
       if (err) {
         console.error(err);
-        res
-          .statusCode(500)
-          .send(
-            "파일을 읽어오는 도중 문제가 발생하였습니다. 다시 시도해주세요."
-          );
+        res.send(
+          "파일을 읽어오는 도중 문제가 발생하였습니다. 다시 시도해주세요."
+        );
       } else {
         res.status(200).send(data);
       }
@@ -49,11 +47,9 @@ app.get("/tweets", (req, res) => {
     fs.readFile(timelineFilePath, "utf-8", (err, data) => {
       if (err) {
         console.error(err);
-        res
-          .statusCode(500)
-          .send(
-            "파일을 읽어오는 도중 문제가 발생하였습니다. 다시 시도해주세요."
-          );
+        res.send(
+          "파일을 읽어오는 도중 문제가 발생하였습니다. 다시 시도해주세요."
+        );
       } else {
         res.status(200).send(data);
       }
@@ -63,16 +59,19 @@ app.get("/tweets", (req, res) => {
 
 function UploadTweet(filePath, tweetObj) {
   if (fs.existsSync(filePath)) {
-    fs.readFile(filePath, "utf-8", (err, data) => {
-      if (err) console.error(err);
-      else {
-        let curr = JSON.parse(data);
-        curr.tweetList.push(tweetObj);
-        fs.writeFile(filePath, JSON.stringify(curr), "utf-8", (err) => {
-          console.error(err);
-        });
+    const file = fs.readFileSync(filePath, "utf-8");
+    const fileData = JSON.parse(file);
+    const tweetList = [...fileData.tweetList];
+    tweetList.push(tweetObj);
+
+    fs.writeFile(
+      filePath,
+      JSON.stringify({ tweetList: tweetList }),
+      "utf-8",
+      (err) => {
+        console.error(err);
       }
-    });
+    );
   } else {
     let obj = {
       tweetList: [tweetObj],
@@ -86,7 +85,6 @@ function UploadTweet(filePath, tweetObj) {
 
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-
   const newTweet = {
     username: username,
     tweet: tweet,
@@ -153,7 +151,6 @@ app.put("/tweets/:id", (req, res) => {
       let curr = JSON.parse(data);
       let index = curr.tweetList.findIndex((x) => x.id === id);
       let prevTweet = curr.tweetList[index];
-      console.log("prev", prevTweet);
       curr.tweetList.splice(index, 1, { ...prevTweet, tweet: newTweet });
       try {
         await fsAsync.writeFile(
