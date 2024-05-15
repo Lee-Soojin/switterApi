a6cc943996c48c19e6a700af6ac02d42c70ab9bc
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-05-15 23:46:06 +0900

    feat: Add tweet controllers

---
+++ controller/tweet.js
@@ -0,0 +1,43 @@
+import * as tweetRepository from "../data/tweet.js";
+
+export async function getTweets(req, res) {
+  const username = req.query?.username;
+
+  const data = await (username
+    ? tweetRepository.getAllByUserName(username)
+    : tweetRepository.getAll());
+  res.status(200).json(data);
+}
+
+export async function getTweet(req, res) {
+  const id = req.params?.id;
+  const data = await tweetRepository.getAllById(id);
+  if (data) res.status(200).json(data);
+  else
+    res
+      .status(404)
+      .json({ message: "해당 아이디를 가진 트윗이 존재하지 않습니다." });
+}
+
+export async function createTweet(req, res) {
+  const { username, tweet } = req.body;
+  const newTweet = await tweetRepository.create(tweet, username);
+  res.status(201).json(newTweet);
+}
+
+export async function updateTweet(req, res) {
+  const { id } = req.params;
+  const text = req.body?.tweet;
+  const newTweet = await tweetRepository.update(text, id);
+  if (newTweet) {
+    res.status(200).json(newTweet);
+  } else {
+    res.status(404).json({ message: "트윗 수정에 실패하였습니다." });
+  }
+}
+
+export async function deleteTweet(req, res) {
+  const { id } = req.body;
+  await tweetRepository.remove(id);
+  res.sendStatus(204);
+}
