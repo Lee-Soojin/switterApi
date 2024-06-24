8d758ce785480e1c931b7da0acfa3adb8a76fa7a
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-06-25 00:01:38 +0900

    feat: Add tweets data handler

--- data/tweet.js
+++ data/tweet.js
@@ -1,28 +1,41 @@
 import uuid4 from "uuid4";
-
+import * as userRepository from "./auth.js";
 let tweets = [];
 
 export async function getAll() {
-  return tweets;
+  return Promise.all(
+    tweets.map(async (tweet) => {
+      const { username, name, url } = await userRepository.findById(
+        tweet.userId
+      );
+      return { ...tweet, username, name, url };
+    })
+  );
 }
+
 export async function getAllByUserName(username) {
   return tweets.filter((x) => x.username === username);
 }
-export async function getAllById(id) {
-  return tweets.find((x) => x.id === id);
+export async function getById(id) {
+  const found = tweets.find((tweet) => tweet.id === id);
+  if (!found) {
+    return null;
+  }
+  const { username, name, url } = await userRepository.findById(found.userId);
+  return { ...found, username, name, url };
 }
 export async function create(text, username) {
   const newTweet = {
     tweet: text,
-    username: username,
     uploadDate: new Date().toString(),
     id: uuid4(),
+    username,
   };
   tweets = [newTweet, ...tweets];
   return tweets;
 }
 
-export async function update(text, id) {
+export async function update(id, text) {
   const tweet = tweets.find((x) => x.id === id);
   if (tweet) tweet.tweet = text;
   return tweet;
