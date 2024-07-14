6e21d5ea07e8298aa4977ba03104d989d31793a8
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-07-14 15:33:11 +0900

    feat: delete tweet

--- controller/tweet.js
+++ controller/tweet.js
@@ -48,6 +48,13 @@ export async function updateTweet(req, res) {
 
 export async function deleteTweet(req, res) {
   const { id } = req.body;
+  const tweet = tweetRepository.getById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
+  }
+  if (tweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
   await tweetRepository.remove(id);
   res.sendStatus(204);
 }
