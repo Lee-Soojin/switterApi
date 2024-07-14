65b3b3e65957ef0e3327a2a0d2a5240cb168b44a
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-07-14 15:33:03 +0900

    feat: update tweet

--- controller/tweet.js
+++ controller/tweet.js
@@ -1,7 +1,7 @@
 import * as tweetRepository from "../data/tweet.js";
 
 export async function getAllTweets(req, res) {
-  const data = tweetRepository.getAll();
+  const data = await tweetRepository.getAll();
   res.status(200).json(data);
 }
 
@@ -25,20 +25,25 @@ export async function getTweet(req, res) {
 }
 
 export async function createTweet(req, res) {
-  const { username, tweet } = req.body;
-  const newTweet = await tweetRepository.create(tweet, username);
+  const { tweet } = req.body;
+  const newTweet = await tweetRepository.create(tweet, req.userId);
   res.status(201).json(newTweet);
 }
 
 export async function updateTweet(req, res) {
   const { id } = req.params;
   const text = req.body?.tweet;
-  const newTweet = await tweetRepository.update(id, text);
-  if (newTweet) {
-    res.status(200).json(newTweet);
-  } else {
-    res.status(404).json({ message: "트윗 수정에 실패하였습니다." });
+  const tweet = await tweetRepository.getById(id);
+
+  if (!tweet) {
+    return res.sendStatus(404);
   }
+  if (tweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
+
+  const updated = await tweetRepository.update(id, text);
+  res.status(200).json(updated);
 }
 
 export async function deleteTweet(req, res) {
