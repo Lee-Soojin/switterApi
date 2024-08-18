2559b88629f8a5b9aef7ab2fe958dbb1349ba138
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-08-18 17:01:50 +0900

    fix: typo

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ import { config } from "../config.js";
 const pool = mysql.createPool({
   host: config.db.host,
   user: config.db.user,
-  database: config.db.database,
+  database: config.db.databse,
   password: config.db.password,
 });
 
