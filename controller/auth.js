9fdb49008719d4dbe6cb5b8575a3f0c95f1d49d5
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2024-09-16 15:28:23 +0900

    feat: Add function profile

--- controller/auth.js
+++ controller/auth.js
@@ -56,3 +56,12 @@ export async function me(req, res, next) {
   }
   res.status(200).json({ token: req.token, ...user });
 }
+
+export async function profile(req, res, next) {
+  const user = await authRepository.findById(req.userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json({ token: req.token, ...user });
+}
